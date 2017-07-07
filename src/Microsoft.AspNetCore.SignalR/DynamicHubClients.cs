858f3980151fe9a19d05f106ee4b029836423479
Mikael Mengistu <mimengis@microsoft.com>
2017-07-06 18:50:08 -0700

    PR Feedback

--- src/Microsoft.AspNetCore.SignalR/DynamicHubClients.cs
+++ src/Microsoft.AspNetCore.SignalR/DynamicHubClients.cs
@@ -5,15 +5,15 @@ namespace Microsoft.AspNetCore.SignalR
 {
     public class DynamicHubClients
     {
-        private IHubClients _inner;
+        private readonly IHubClients _clients;
 
         public DynamicHubClients(IHubClients clients)
         {
-            _inner = clients;
+            _clients = clients;
         }
-        public dynamic All => new DynamicClientProxy(_inner.All);
-        public dynamic User(string userId) => new DynamicClientProxy(_inner.User(userId));
-        public dynamic Group(string group) => new DynamicClientProxy(_inner.Group(group));
-        public dynamic Client(string connectionId) => new DynamicClientProxy(_inner.Client(connectionId));
+        public dynamic All => new DynamicClientProxy(_clients.All);
+        public dynamic User(string userId) => new DynamicClientProxy(_clients.User(userId));
+        public dynamic Group(string group) => new DynamicClientProxy(_clients.Group(group));
+        public dynamic Client(string connectionId) => new DynamicClientProxy(_clients.Client(connectionId));
     }
 }
