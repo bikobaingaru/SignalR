1d3d11f51267d3d38e9d94af4c66da0845058b83
Mikael Mengistu <mimengis@microsoft.com>
2017-07-06 18:05:17 -0700

    Dynamic Hub Change part 2

--- samples/SocketsSample/Hubs/Chat.cs
+++ samples/SocketsSample/Hubs/Chat.cs
@@ -7,45 +7,45 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SocketsSample.Hubs
 {
-    public class Chat : Hub
+    public class Chat : DynamicHub
     {
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} joined");
+            await Clients.All.Send($"{Context.ConnectionId} joined");
         }
 
         public override async Task OnDisconnectedAsync(Exception ex)
         {
-            await Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} left");
+            await Clients.All.Send($"{Context.ConnectionId} left");
         }
 
         public Task Send(string message)
         {
-            return Clients.All.InvokeAsync("Send", $"{Context.ConnectionId}: {message}");
+            return Clients.All.Send($"{Context.ConnectionId}: {message}");
         }
 
         public Task SendToGroup(string groupName, string message)
         {
-            return Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId}@{groupName}: {message}");
+            return Clients.Group(groupName).Send($"{Context.ConnectionId}@{groupName}: {message}");
         }
 
         public async Task JoinGroup(string groupName)
         {
             await Groups.AddAsync(Context.ConnectionId, groupName);
 
-            await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} joined {groupName}");
+            await Clients.Group(groupName).Send($"{Context.ConnectionId} joined {groupName}");
         }
 
         public async Task LeaveGroup(string groupName)
         {
             await Groups.RemoveAsync(Context.ConnectionId, groupName);
 
-            await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} left {groupName}");
+            await Clients.Group(groupName).Send($"{Context.ConnectionId} left {groupName}");
         }
 
         public Task Echo(string message)
         {
-            return Clients.Client(Context.ConnectionId).InvokeAsync("Send", $"{Context.ConnectionId}: {message}");
+            return Clients.Client(Context.ConnectionId).Send($"{Context.ConnectionId}: {message}");
         }
     }
 }
---
+++ src/Microsoft.AspNetCore.SignalR/DynamicHubClients.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.SignalR
+{
+    public class DynamicHubClients
+    {
+        private IHubClients _inner;
+
+        public DynamicHubClients(IHubClients clients)
+        {
+            _inner = clients;
+        }
+        public dynamic All => new DynamicClientProxy(_inner.All);
+        public dynamic User(string userId) => new DynamicClientProxy(_inner.User(userId));
+        public dynamic Group(string group) => new DynamicClientProxy(_inner.Group(group));
+        public dynamic Client(string connectionId) => new DynamicClientProxy(_inner.Client(connectionId));
+    }
+}
